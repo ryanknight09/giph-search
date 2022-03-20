8be1457d0180868b071b38f068e85ba0b96ad38c
Ryan Knight <ryan.knight09@outlook.com>
2022-03-19 20:08:54 -0700

    api key env vars

--- apps/geo-ip/src/api/useFetchGif.tsx
+++ apps/geo-ip/src/api/useFetchGif.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
+const API_KEY = process.env['NX_REACT_APP_API_KEY'];
+
 const useFetchGif = (searchKey: string) => {
   let safeKey = '';
   if (searchKey.length < 50) safeKey = searchKey;
@@ -11,7 +13,7 @@ const useFetchGif = (searchKey: string) => {
     ['searchKey', searchKey],
     async () => {
       const { data } = await axios.get(
-        `${BASE_URL}/gifs/search?api_key=hIf8Orkj8zJNLoSSigelOoD2c8mpO0s0&q=${safeKey}&limit=3&offset=0&lang=en`
+        `${BASE_URL}/gifs/search?api_key=${API_KEY}&q=${safeKey}&limit=3&offset=0&lang=en`
       );
       return data;
     },
